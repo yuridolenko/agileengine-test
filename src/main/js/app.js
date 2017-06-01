1365b2a7569b6644ec3b8a82d0f289247b9e2b27
ded <yuri.dolenko@gmail.com>
2017-06-02 00:02:38 +0300

    js updated

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,8 +4,99 @@ const React = require('react');
 const ReactDOM = require('react-dom')
 const client = require('./client');
 
-class App extends React.Component {
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {moneyAmount: 0, accountId: 0};
+    }
+
+    render() {
+        return (
+            <div className="verticalContainer">
+                <div className="container">
+                    <AccountDropDownList onChange={this.onAccountChanged}/>
+                    <input type="number" onChange={this.onMoneyChanged.bind(this)}/>
+                    <MoneyButton caption="Credit" onClick={this.onMoneyButtonClick.bind(this, 'CREDIT')}/>
+                    <MoneyButton caption="Dedit" onClick={this.onMoneyButtonClick.bind(this, 'DEBIT')}/>
+                </div>
+                <TransactionsTable />
+            </div>
+        )
+    }
+
+    onAccountChanged(e) {
+        this.setState({accountId: e.target.value});
+    }
+
+    onMoneyChanged(e) {
+        this.setState({moneyAmount: e.target.value});
+    }
+
+    onMoneyButtonClick(type) {
+        let transaction = {accountId: this.state.accountId, type: type, amount: this.state.moneyAmount}
+        client({method: 'PUT', path: '/api/transactions', entity: transaction}).done(response => {
+            console.log(response)
+        });
+    }
+}
+
+class TransactionsTable extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {transactions: []};
+    }
+
+    refresh() {
+        let self = this;
+        client({method: 'GET', path: '/api/transactions'}).done(response => {
+            self.setState({transactions: response.entity});
+        });
+    }
+
+    componentDidMount() {
+        this.refresh();
+    }
+
+    render() {
+        var transactions = this.state.transactions.map(transaction =>
+            <Transaction key={transaction.id} transaction={transaction}/>
+        );
+        return (
+            <div className="verticalContainer">
+                <button type="button" onClick={this.refresh.bind(this)} className="refreshTransactionButton">Refresh</button>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Id</th>
+                        <th>Account Name</th>
+                        <th>Type</th>
+                        <th>Amount</th>
+                    </tr>
+                    {transactions}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+class Transaction extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.transaction.id}</td>
+                <td>{this.props.transaction.accountName}</td>
+                <td>{this.props.transaction.type}</td>
+                <td>{this.props.transaction.amount}</td>
+            </tr>
+        )
+    }
+}
+
+
+class AccountDropDownList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {accounts: []};
@@ -18,19 +109,11 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <AccountDropDownList accounts={this.state.accounts}/>
-        )
-    }
-}
-
-class AccountDropDownList extends React.Component {
-    render() {
-        var accounts = this.props.accounts.map(account =>
+        var accounts = this.state.accounts.map(account =>
             <Account key={account.id} account={account}/>
         );
         return (
-            <select>
+            <select onChange={this.props.onChange}>
                 {accounts}
             </select>
         )
@@ -45,8 +128,15 @@ class Account extends React.Component {
     }
 }
 
+class MoneyButton extends React.Component {
+    render() {
+        return (
+            <button type="button" onClick={this.props.onClick}>{this.props.caption}</button>
+        )
+    }
+}
+
 ReactDOM.render(
     <App />,
-    document.getElementById('react')
+    document.getElementById('root')
 )
-
